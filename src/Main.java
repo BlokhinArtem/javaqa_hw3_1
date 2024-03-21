dbe7dff6eb6c55d414bcdf21e3974247ac302015
Artem Blokhin <rtmdragee@gmail.com>
2024-03-22 00:36:34 +0400

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
+        bonusMilesService service = new bonusMilesService();
         int price = 20_000;
         int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
